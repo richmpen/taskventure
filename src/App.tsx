fa6713291f10b9986d9e3cbdc2f0f42bb58b5e5a
Oner <user@example.com>
2025-04-01 04:52:53 +0300

    Исправлен роутинг для GitHub Pages, изменен homepage в package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ThemeProvider from './context/ThemeContext';
 import PrivateRoute from './components/PrivateRoute';
